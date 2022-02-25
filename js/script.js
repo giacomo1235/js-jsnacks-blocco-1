ace6bb0a56731320625adc92692b5e141d7efa5c
giacomo1235 <giacomo.passariello@gmail.com>
2022-02-25 15:19:27 +0100

    primi tre snacks

---
+++ js/script.js
@@ -0,0 +1,48 @@
+/* snack 1 */
+/* 
+const num1 = parseInt(prompt('primo numero'));
+const num2 = parseInt(prompt('secondo numero'));
+
+if (num1 > num2) {
+    document.getElementById('maggiore').innerHTML = `il primo numero è più grande`
+} else if (num1 < num2) {
+    document.getElementById('maggiore').innerHTML = `il secondo numero è più grande`
+} else {
+    document.getElementById('maggiore').innerHTML = `sono uguali`
+};
+ */
+/* / snack 1 */
+
+/* snack 2 */
+/*
+let string1 = prompt('prima parola');
+let string2 = prompt('seconda parola');
+
+if (string1.length > string2.length) {
+    document.getElementById('maggiore').innerHTML = `la prima parola è più lunga`
+} else if (string1.length < string2.length) {
+    document.getElementById('maggiore').innerHTML = `la seconda parola è più lunga`
+} else {
+    document.getElementById('maggiore').innerHTML = `sono lunghe uguali`
+};
+*/
+/* / snack2 */
+
+/* snack 3  */
+/* 
+let arrNum = [];
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+    let userNum = parseInt(prompt('numero'));
+    arrNum.push(userNum);
+}
+
+for (let i=0; i < arrNum.length; i++) {
+    somma += arrNum[i];
+     
+};
+
+document.getElementById('maggiore').innerHTML = somma;
+*/
+/* / snack 3 */
